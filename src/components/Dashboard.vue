b9589fd9f251b1bac68522c6dbaeb2821b487ee9
Fida ur Rahman <fidaurr900@gmail.com>
2024-04-19 18:24:36 +0500

    Update HomeView.vue, package.json, main.css, HelloWorld.vue, WelcomeItem.vue, and IconCommunity.vue

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,4 +1,6 @@
 <script setup>
+import IconCommunity from './icons/IconCommunity.vue';
+import CarretUp from './icons/CarretUp.vue';
 </script>
 
 <template>
@@ -16,6 +18,38 @@
             <div class="searchManu">khan</div>
         </div>
 
+        <div class="counting">
+            <div class="tasks">
+                <p>Total project <span class="greenText">
+                        <CarretUp /> 2
+                    </span></p>
+                <h1 class="boldNumber">7</h1>
+            </div>
+            <div class="tasks">
+                <p>Total Tasks<span class="greenText">
+                        <b-icon icon="bar-chart-fill"></b-icon>4
+                    </span></p>
+                <h1 class="boldNumber">49</h1>
+            </div>
+            <div class="tasks">
+                <p>Assigned Tasks<span class="redText">
+                        <ArrowDropDownIcon />3
+                    </span></p>
+                <h1 class="boldNumber">12</h1>
+            </div>
+            <div class="tasks">
+                <p>Total project <span class="greenText">
+                        <ArrowDropUpIcon />1
+                    </span></p>
+                <h1 class="boldNumber">6</h1>
+            </div>
+            <div class="tasks">
+                <p>Total project <span class="greenText">
+                        <ArrowDropUpIcon />2
+                    </span></p>
+                <h1 class="boldNumber">3</h1>
+            </div>
+        </div>
     </div>
 
 </template>
@@ -73,6 +107,43 @@ body {
     gap: 0px;
     opacity: 0px;
 
+}
+
+.counting {
+    display: flex;
+    border: collapse;
+    border: 1px solid rgb(234, 228, 228);
+    border-radius: 10px;
+    justify-content: space-between;
+    margin-top: 2vh;
+    margin-left: 3vw;
+    margin-right: 3vw;
+    /* padding: 1rem; */
+}
+
+.tasks {
+    width: 17vw;
+    height: 13vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 15px;
+    border: collapse;
+    border: 1px solid rgb(234, 228, 228);
+    border-radius: 10px;
+}
+
+.greenText {
+    color: #2CC171;
+}
+
+.redText {
+    color: #FF0000;
+}
+
+.boldNumber {
+    font-size: 2rem;
+    font-weight: 700;
 }
 
 @media only screen and (max-width: 768px) {}
